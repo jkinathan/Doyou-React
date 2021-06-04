98ede073439d7f0cbbf8dd156b83c1b012888d6d
jkinathan <jkinobe@gmail.com>
2021-06-04 10:17:49 +0300

    Components

---
+++ helloworld/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// function Greet(){
+//     return <h1> Hello Jordan</h1>
+// }
+
+const Greet = ()=> <h1>Hello Jordy!</h1>
+// or
+// export const Greet = ()=> <h1>Hello Jordy!</h1>
+
+
+
+export default Greet;

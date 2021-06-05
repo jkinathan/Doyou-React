4f9150aeb450fe979b7d161ad599dac25b9d2736
jkinathan <jkinobe@gmail.com>
2021-06-05 11:39:15 +0300

    function as an arg to access previous state in setState

--- helloworld/src/components/Counter.js
+++ helloworld/src/components/Counter.js
@@ -9,19 +9,31 @@ class Counter extends Component {
         }
     }
     addMe(){
-        this.setState({
-            count: this.state.count+1
-        },
-        // call back function - execute some code only after state has been updated
-        // execute some code after the state has changed
-        ()=> { console.log('Call back value:',this.state.count )})
+        // this.setState({
+        //     count: this.state.count+1
+        // },
+        // // call back function - execute some code only after state has been updated
+        // // execute some code after the state has changed
+        // ()=> { console.log('Call back value:',this.state.count )})
+
+        this.setState((previousState,props)=>({
+            count: previousState.count + 1
+        }))
+    }
+
+    addFive(){
+        this.addMe()
+        this.addMe()
+        this.addMe()
+        this.addMe()
+        this.addMe()
     }
     render() {
         return (
             <div>
                 Count - {this.state.count}
                 <br></br>
-                <button onClick={()=>this.addMe()}>Add</button>               
+                <button onClick={()=>this.addFive()}>Add</button>               
             </div>
         )
     }

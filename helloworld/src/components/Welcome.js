87aa75987c463ad13897876ec98c10c36cd42417
jkinathan <jkinobe@gmail.com>
2021-06-07 09:12:07 +0300

    destructuring props for class components

--- helloworld/src/components/Welcome.js
+++ helloworld/src/components/Welcome.js
@@ -2,11 +2,14 @@
 import React, { Component } from 'react';
 
 class Welcome extends Component{
+    // destructuring props in the class components
     render(){
+        const { name,heroname } = this.props
+        // simply extracting the necessary props
         return (
                 <div>
-                    <h1>Welcome { this.props.name } Kinda { this.props.heroname }</h1>
-                    <h3>{ this.props.children }</h3>
+                    <h1>Welcome { name } Kinda { heroname }</h1>
+                    {/* <h3>{ this.props.children }</h3> */}
                 </div>
             );
     }

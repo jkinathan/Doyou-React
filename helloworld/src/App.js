f1e47352c720b6f310efa65fd2165003c6ff0cfa
jkinathan <jkinobe@gmail.com>
2021-06-08 10:49:06 +0300

    pass methods as props to child from parent

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -4,6 +4,8 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import Parent from './components/Parent';
+
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
         <div>
             {/* <FunctionClick></FunctionClick> */}
             {/* <ClassClick></ClassClick> */}
-            <EventBind></EventBind>
+            {/* <EventBind></EventBind> */}
+            <Parent></Parent>
         </div>
         
       }
---
+++ helloworld/src/components/Parent.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Child from './Child';
+
+class Parent extends Component{
+    
+    constructor(){
+    super()
+        this.state = {
+            ParentName: "Parent"
+        }
+        this.greetParent = this.greetParent.bind(this)
+    }
+
+    greetParent(childName){
+        // alert('Hello'+this.state.ParentName)
+        // ES6
+        alert(`Hello ${this.state.ParentName} from ${childName}`)
+    }
+    render(){
+        return(
+            <div>
+                <Child greetHandler={this.greetParent}></Child>
+            </div>
+        )
+    }
+}
+
+export default Parent

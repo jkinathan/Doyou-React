3a7155c7374ac52752a62b6a5716045a9ee7d341
jkinathan <jkinobe@gmail.com>
2021-07-04 12:47:09 +0300

    conditionally running useEffect hook

--- state-hook/src/components/UseEffect/CounterOne.js
+++ state-hook/src/components/UseEffect/CounterOne.js
@@ -3,15 +3,21 @@ import React, {useState, useEffect} from 'react'
 function CounterOne() {
 
     const [count,setCount] = useState(0)
+    const [name,setName] = useState('')
 
     //useEffect accepts a function as a parameter or argument
     //causing sideeffects from a functional component using effect hook
     useEffect(()=>{
         document.title = `You click ${count}`;
-    })
+        console.log('Updating....')
+    },[count])
+
+    //what is included in the second argument is prop or state which if it changes
+    //is when the useEffect will be run
 
     return (
         <div>
+            <input type="text" value={name} onChange={(event)=>setName(event.target.value)}/>
             <button onClick={()=>setCount(count-1)}>Count:{count}</button>
         </div>
     )

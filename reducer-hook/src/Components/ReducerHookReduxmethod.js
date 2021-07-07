28a615a62d33dd4603203fe75be72e06619870ee
jkinathan <jkinobe@gmail.com>
2021-07-07 14:53:21 +0300

    reducer multipleState update

--- reducer-hook/src/Components/ReducerHookReduxmethod.js
+++ reducer-hook/src/Components/ReducerHookReduxmethod.js
@@ -2,7 +2,8 @@ import React, {useReducer} from 'react'
 
 
 const initialState = {
-    firststate: 0
+    firststate: 0,
+    Countstate2 : 0
 }
 const reducer = (currentState, action)=>{ //reducer function accepts also two parameters currentState
     // and second the action to be done on the current state hehehe
@@ -10,15 +11,24 @@ const reducer = (currentState, action)=>{ //reducer function accepts also two pa
     switch(action.type){
         case 'increment':
             return {
-                firststate: currentState.firststate + 1
+                ...currentState,
+                firststate: currentState.firststate + action.value
             }
         case 'decrement':
             return {
-                firststate: currentState.firststate -1 }
+                ...currentState,
+                firststate: currentState.firststate - action.value }
+        case 'Secondincrement':
+            return {
+                ...currentState,
+                Countstate2: currentState.Countstate2 + action.value
+            }
+        case 'Seconddecrement':
+            return {
+                ...currentState,
+                Countstate2: currentState.Countstate2 - action.value }
         case 'reset':
-            return {    
-                initialState
-                }
+            return initialState
         default:
             return {currentState}
     }
@@ -36,10 +46,27 @@ function ReducerHookReduxmethod() {
         <div>
 
             Count: {newCount.firststate}
+            <br>
+            </br>
             
-            <button onClick={()=>dispatch({ type: 'increment' })}>Increment</button>
-            <button onClick={()=>dispatch({ type: 'decrement' })}>Decrement</button>
-            <button onClick={()=>dispatch({ type: 'reset' })}>Reset</button>
+            <button onClick={()=>dispatch({ type: 'increment', value: 1 })}>Increment</button>
+            <button onClick={()=>dispatch({ type: 'decrement', value:1 })}>Decrement</button>
+
+            <button onClick={()=>dispatch({ type: 'increment', value: 5 })}>Increment by 5</button>
+            <button onClick={()=>dispatch({ type: 'decrement', value: 5 })}>Decrement by 5</button>
+            
+            <br>
+            </br>
+            Second Counter : {newCount.Countstate2}
+            <br>
+            </br>
+            
+            <button onClick={()=>dispatch({ type: 'Secondincrement', value: 10 })}>Secondincrement by 10</button>
+            <button onClick={()=>dispatch({ type: 'Seconddecrement', value: 10 })}>Secondincrement by 10</button>
+
+
+
+<button onClick={()=>dispatch({ type: 'reset' })}>Reset</button>
 
 
         </div>

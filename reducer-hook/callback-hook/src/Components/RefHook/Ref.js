fdf8f41758764bd890cc2289768c9574a652fc66
jkinathan <jkinobe@gmail.com>
2021-07-09 09:58:05 +0300

    useRef hook for access Dom nodes within func components

---
+++ reducer-hook/callback-hook/src/Components/RefHook/Ref.js
@@ -0,0 +1,21 @@
+import React,{useRef, useEffect} from 'react'
+
+function Ref() {
+
+    const inputRef = useRef(null)
+    //pass in the initial value
+
+    useEffect(()=>{
+        inputRef.current.focus()
+        //run it once as soon as the page is loaded
+    },[])
+
+    return (
+        <div>
+            <input ref={inputRef} type="text"/>
+            //use ref keyword for attaching the created ref to the input field
+        </div>
+    )
+}
+
+export default Ref
